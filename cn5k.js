6afea1630b371ff952f989ee186141697c127da3
Lars Woldern <lw222ii@student.lnu.se>
2016-04-17 10:24:42 +0800

    project view added

--- cn5k.js
+++ cn5k.js
@@ -1,2 +1,20 @@
 "use strict";
 
+let handlebars = require("express-handlebars").create({defaultLayout: "main"});
+let express = require("express");
+
+let app = express();
+
+app.use(express.static(__dirname + "public"));
+
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
+app.set("port", process.env.PORT || 3000);
+
+//------    ROUTES  ------//
+app.use("/", require("./routes/main.js"));
+
+//------    BOOT    ------//
+app.listen(app.get("port"), function() {
+    console.log("Express started on http://localhost:" + app.get("port") + "; press Ctrl-C to terminate.");
+});
---
+++ routes/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let router = require("express").Router();
+
+router.route("/")
+    .get(function(req, res) {
+        res.render("home")
+    });
+
+module.exports = router;
